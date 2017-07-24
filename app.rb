ffe1d4fcfabd09dcbc74645677382b2b27a35c2d
memetibryan <memetibryan@gmail.com>
2017-07-24 12:11:26 +0300

    added code to save new host and event

--- app.rb
+++ app.rb
@@ -9,7 +9,55 @@ require("bundler/setup")
     erb(:index)
   end
 
+  get("/event") do
+  	@events = Event.all()
+  	erb(:events)
+  end
+  
+
+  get("/host") do
+    @hosts = Host.all()
+    erb(:hosts)
+  end
+
   get("/events") do
   	@events = Event.all()
-  	erb(:Events)
+  	erb(:events)
+  end
+
+  get("/hosts") do
+    @hosts = Host.all()
+    erb(:hosts)
+  end
+
+  get("/event/new") do 
+  	erb(:event_form)
+  end
+
+  get("/host/new") do 
+    erb(:host_form)
+  end
+
+  post("/events") do
+  	name = params.fetch(:name)
+  	description = params.fetch(:description)
+  	number_of_tickets = params.fetch(:number_of_tickets)
+  	event = Event.new({:name => name, :description => description, :number_of_tickets => number_of_tickets, :id => nil})
+  	if event.save()
+  		redirect("/events")
+  	else
+  		erb(:errors)
+  	end
+  end
+
+  post("/hosts") do
+    name = params.fetch(:name)
+    Telephone = params.fetch(:Telephone)
+    Email = params.fetch(:Email)
+    host = Host.new({:name => name, :Telephone => Telephone, :Email => Email, :id => nil})
+    if host.save()
+      redirect("/hosts")
+    else
+      erb(:errors)
+    end
   end
--- lib/Event.rb
+++ lib/Event.rb
@@ -1,3 +1,15 @@
 class Event < ActiveRecord::Base
 	belongs_to(:host)
+
+	#Active Record Validations making sure the form is not submitted blank
+	validates(:name, :presence => true)
+
+	#changes the input to Title_Case
+	before_save(:titlecase_name)
+
+private
+
+    define_method(:titlecase_name) do
+      self.name = (name().titlecase())
+    end
 end
