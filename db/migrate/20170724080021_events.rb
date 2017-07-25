c1387deb507024605662d0e5e836e5c1f31558c2
memetibryan <memetibryan@gmail.com>
2017-07-25 16:13:48 +0300

    changed description datatype to from varchar to text

--- db/migrate/20170724080021_events.rb
+++ db/migrate/20170724080021_events.rb
@@ -3,7 +3,7 @@ class Events < ActiveRecord::Migration[5.1]
   	create_table(:events) do |t|
   		t.column(:host_id, :integer)
   		t.column(:name, :string)
-  		t.column(:description, :string)
+  		t.column(:description, :text)
   		t.column(:number_of_tickets, :bigint)
 
   		t.timestamps()
